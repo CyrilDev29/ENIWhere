ab17920f1c98c652a057df4bbfd746e2d64a57d2
MarianaV92 <marivitikhovich92@gmail.com>
2025-08-26 23:42:29 +0200

    Refactor user entity and forms; add admin user management and main page

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,31 +5,80 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Repository\UserRepository;
-use App\Security\EmailVerifier;
 use App\Security\LoginFormAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mime\Address;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
-use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
+
 
 class RegistrationController extends AbstractController
+
 {
-    public function __construct(private EmailVerifier $emailVerifier)
+ // a renommer les fichiers ce controller sera AdminController
+
+    #[Route('/admin/users', name: 'admin_user_list')]
+    public function list(UserRepository $userRepository): Response
     {
+
+        //$this->denyAccessUnlessGranted('ROLE_ADMIN');
+        $users = $userRepository->findAll();
+
+        return $this->render('registration/list.html.twig', [
+            'users' => $users,
+        ]);
     }
 
-    #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
+
+
+    #[Route('/admin/users/update/{id}', name: 'admin_user_edit',requirements: ['id' => '\d+'])]
+    public function edit(User $user,Request $request,EntityManagerInterface $entityManager,UserPasswordHasherInterface $userPasswordHasher): Response
     {
+
+        $form = $this->createForm(RegistrationFormType::class, $user, [
+            'is_registration'=>false,
+            'is_admin'=>true,
+
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $plainPassword = $form->get('plainPassword')->getData();
+            if ($plainPassword) {
+                $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
+            }
+            $user->setIsActive(true);
+            $email = trim($form->get('email')->getData());
+            $user->setEmail($email);
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Utilisateur mis à jour avec succès.');
+
+            return $this->redirectToRoute('admin_user_list');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form,
+        ]);
+    }
+
+
+    #[Route('/admin/user/create', name: 'admin_user_create')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher,  EntityManagerInterface $entityManager): Response
+    {
+
+       // $this->denyAccessUnlessGranted('ROLE_ADMIN'); A activer je desactive que pour les tests
         $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form = $this->createForm(RegistrationFormType::class, $user, [
+            'is_registration'=>true,
+            'is_admin'=>true,
+
+        ]);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -38,22 +87,16 @@ class RegistrationController extends AbstractController
 
             // encode the plain password
             $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
-
+            $user->setIsActive(true);
+            $email = trim($form->get('email')->getData());
+            $user->setEmail($email);
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-                (new TemplatedEmail())
-                    ->from(new Address('mailer@eniwhere.com', 'tintin'))
-                    ->to((string) $user->getEmail())
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
-
+            $this->addFlash('success', 'User created successfully.');
             // do anything else you need here, like send an email
 
-            return $security->login($user, LoginFormAuthenticator::class, 'main');
+            return $this->redirectToRoute('admin_user_list');
         }
 
         return $this->render('registration/register.html.twig', [
@@ -61,33 +104,5 @@ class RegistrationController extends AbstractController
         ]);
     }
 
-    #[Route('/verify/email', name: 'app_verify_email')]
-    public function verifyUserEmail(Request $request, TranslatorInterface $translator, UserRepository $userRepository): Response
-    {
-        $id = $request->query->get('id');
 
-        if (null === $id) {
-            return $this->redirectToRoute('app_register');
-        }
-
-        $user = $userRepository->find($id);
-
-        if (null === $user) {
-            return $this->redirectToRoute('app_register');
-        }
-
-        // validate email confirmation link, sets User::isVerified=true and persists
-        try {
-            $this->emailVerifier->handleEmailConfirmation($request, $user);
-        } catch (VerifyEmailExceptionInterface $exception) {
-            $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
-
-            return $this->redirectToRoute('app_register');
-        }
-
-        // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
-
-        return $this->redirectToRoute('app_login');
-    }
 }
