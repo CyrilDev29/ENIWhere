0509b4fb1e23cc31d0b262fee63c23eb57ce662f
MarianaV92 <marivitikhovich92@gmail.com>
2025-09-01 22:35:14 +0200

    Refactor code

--- src/Controller/CityController.php
+++ src/Controller/CityController.php
@@ -12,6 +12,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
+
 #[Route('/cities', name: 'city_')]
 final class CityController extends AbstractController
 {
@@ -23,48 +25,52 @@ final class CityController extends AbstractController
         ]);
     }
 
+    #[Route('/cities/new', name: 'new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $city = new City();
+        $form = $this->createForm(CityType::class, $city);
+        $form->handleRequest($request);
 
-#[Route('/cities/new', name: 'new', methods: ['GET', 'POST'])]
-public function new(Request $request, EntityManagerInterface $em): Response
-{
-    $city = new City();
-    $form = $this->createForm(CityType::class, $city)->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($city);
+            $em->flush();
 
-    if ($form->isSubmitted() && $form->isValid()) {
-        $em->persist($city);
-        $em->flush();
-        $this->addFlash('success', 'Ville créée avec success!');
-        return $this->redirectToRoute('app_city_index');
+            $this->addFlash('success', 'Ville créée avec succès !');
+            return $this->redirectToRoute('city_index');
+        }
+
+        return $this->render('city/new.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
-    return $this->render('city/new.html.twig', ['form' => $form->createView()]);
-}
 
-#[Route('cities/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
-public function edit( City $city, Request $request, EntityManagerInterface $em): Response
-{
-    $form = $this->createForm(CityType::class, $city)->handleRequest($request);
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(City $city, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(CityType::class, $city)->handleRequest($request);
 
-    if ($form->isSubmitted() && $form->isValid()) {
-        $em->flush();
-        $this->addFlash('success', 'Ville mise à jour avec success!');
-        return $this->redirectToRoute('app_city_index');
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'Ville mise à jour avec succès!');
+            return $this->redirectToRoute('city_index');
+        }
 
+        return $this->render('city/edit.html.twig', [
+            'form' => $form->createView(),
+            'city' => $city,
+        ]);
     }
-    return $this->render('city/edit.html.twig', [
-        'form' => $form->createView(),
-        'city' => $city,
-    ]);
-}
 
-#[Route('/{id}', name: 'delete', methods: ['POST'])]
-public function delete(City $city, Request $request, EntityManagerInterface $em): Response
-{
-    if ($this->isCsrfTokenValid('delete_city_'.$city->getId(), $request->request->get('_token'))) {
-    $em->remove($city);
-    $em->flush();
-    $this->addFlash('success', 'Vile supprimer avec success!');
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
+    public function delete(City $city, Request $request, EntityManagerInterface $em): Response
+    {
+        if ($this->isCsrfTokenValid('delete_city_'.$city->getId(), $request->request->get('_token'))) {
+            $em->remove($city);
+            $em->flush();
+            $this->addFlash('success', 'Ville supprimée avec succès!');
+        }
+
+        return $this->redirectToRoute('city_index');
     }
-    return $this->redirectToRoute('app_city_index');
 }
-}
-
